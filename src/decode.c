966fcccd4556a7b1280aaf91ac296ebb27d552ea
Ryan Pavlik <rpavlik@gmail.com>
2012-09-20 17:26:17 -0500

    Basic support for other types

--- src/decode.c
+++ src/decode.c
@@ -121,6 +121,14 @@ void cpk_err(cpk_object_t *obj, uint32_t code, char *reason,
     } \
 }
 
+#define READN(len,src,dest,err) \
+{   cpk_input_t *_in = (src); \
+    if(cpk_read_bytes(_in,(dest),(len)) < 0) { \
+        cpk_err((err), CPK_ERR_EOF, CPK_ERR_EOF_MSG, 0, _in->buffer_read); \
+        return; \
+    } \
+}
+
 #define DECODE_TEST(in,dest,test,err) \
 {   cpk_input_t *__in = (in); \
     cpk_object_t *__err = (err); \
@@ -131,14 +139,14 @@ void cpk_err(cpk_object_t *obj, uint32_t code, char *reason,
     if(CPK_IS_ERROR(dest->header)) { \
         *__err = *dest; \
         free(dest); \
-        return; \
+        dest = NULL; \
     } \
  \
     if(!test(dest->header)) { \
         cpk_err(__err, CPK_ERR_BAD_TYPE, CPK_ERR_BAD_TYPE_MSG, \
                 dest->header, __in->buffer_read); \
         free(dest); \
-        return; \
+        dest = NULL; \
     } \
 }
 
@@ -177,7 +185,10 @@ void cpk_decode_number(cpk_input_t *in, cpk_object_t *obj, uint8_t h) {
         case CPK_COMPLEX:
         case CPK_RATIONAL:
             DECODE_TEST(in, a, CPK_IS_NUMBER, obj);
+            if(!a) return;
+            
             DECODE_TEST(in, b, CPK_IS_NUMBER, obj);
+            if(!b) { cpk_free(b); free(b); return; }
 
             obj->complex.r = a;
             obj->complex.i = b;
@@ -187,6 +198,19 @@ void cpk_decode_number(cpk_input_t *in, cpk_object_t *obj, uint8_t h) {
             cpk_err(obj, CPK_ERR_BAD_HEADER, CPK_ERR_BAD_HEADER_MSG,
                     h, in->buffer_read);
     }            
+}
+
+uint32_t
+cpk_decode_size(cpk_input_t *in, uint8_t header, cpk_object_t *err) {
+    uint8_t i8;
+    uint16_t i16;
+    uint32_t i32;
+
+    switch(header & CPK_SIZE_MASK) {
+        case CPK_SIZE_8:  READ(8, in, &i8, err); return i8;
+        case CPK_SIZE_16: READ(16, in, &i16, err); return i16;
+        case CPK_SIZE_32: READ(32, in, &i32, err); return i32;
+    }
 }
 
 void cpk_decode(cpk_input_t *in, cpk_object_t *obj) {
@@ -199,6 +223,32 @@ void cpk_decode(cpk_input_t *in, cpk_object_t *obj) {
         READ(8, in, &obj->bool.val, obj);
     } else if(CPK_IS_NUMBER(header)) {
         cpk_decode_number(in, obj, header);
+    } else if(CPK_IS_CONTAINER(header)) {
+        obj->container.size = cpk_decode_size(in, header, obj);
+        if(header & CPK_CONTAINER_FIXED)
+            READ(8, in, &obj->container.fixed_header, obj);
+    } else if(CPK_IS_STRING(header)) {
+        obj->string.size = cpk_decode_size(in, header, obj);
+        obj->string.data = malloc(obj->string.size+1);
+        READN(obj->string.size, in, obj->string.data, obj);
+        obj->string.data[obj->string.size] = 0;
+    } else if(CPK_IS_REF(header) ||
+              CPK_IS_TAG(header) ||
+              CPK_IS_INDEX(header)) {
+        if(header & CPK_REFTAG_INLINE)
+            obj->ref.val = header & 0xF;
+        else
+            obj->ref.val = cpk_decode_size(in, header, obj);
+    } else if(CPK_IS_REMOTE_REF(header)) {
+        obj->rref.val = NULL;
+    } else if(CPK_IS_CONS(header)) {
+        obj->cons.car = NULL;
+        obj->cons.cdr = NULL;
+    } else if(CPK_IS_PACKAGE(header)) {
+        obj->package.name = NULL;
+    } else if(CPK_IS_SYMBOL(header)) {
+        obj->symbol.name = NULL;
+        obj->symbol.package = NULL;
     }
 }
 
@@ -212,5 +262,10 @@ void cpk_free(cpk_object_t *obj) {
             free(obj->complex.r);
             free(obj->complex.i);
         }
+    } else if(CPK_IS_STRING(obj->header)) {
+        free(obj->string.data);
+    } else if(CPK_IS_REMOTE_REF(obj->header)) {
+        cpk_free(obj->rref.val);
+        free(obj->rref.val);
     }
 }
