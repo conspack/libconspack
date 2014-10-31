e4cc52248b85056a51080f48f77204d9c3596a77
Ryan Pavlik <rpavlik@gmail.com>
2014-10-30 19:54:50 -0500

    Update autotools stuff

--- config.sh
+++ config.sh
@@ -4,9 +4,10 @@
 
 # Project name
 export AUTO_PROJECT="libconspack"
+export AUTO_PROJECT_VERSION="0.0.0"
 
 # Required autotools versions
-export REQ_AUTOCONF="2.53"
-export REQ_AUTOMAKE="1.12.4"
-export REQ_LIBTOOL="1.4.2"
-
+export REQ_AUTOHEADER="2.69"
+export REQ_AUTOCONF="2.69"
+export REQ_AUTOMAKE="1.13.1"
+export REQ_LIBTOOL="2.4.2"
